46d16edfe17d8ad5b07de9c66867f08027ef82da
probabl <hsfk2017@gmail.com>
2025-08-11 20:10:06 +0100

    Setup for switching camera angles and sitting in chairs

--- Assets/Scripts/Clicker_Player.cs
+++ Assets/Scripts/Clicker_Player.cs
@@ -1,42 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 
 public class Clicker_Player : MonoBehaviour
 {
-    [SerializeField] private InputActionReference click_Action;
-    [SerializeField] private InputActionReference cursor_Action;
+    [SerializeField] public InputActionReference map_Click_Action;
+    [SerializeField] public InputActionReference map_Cursor_Action;
     [SerializeField] private float move_Speed;
-    private Camera cam;
+    public Camera cam;
     private Vector3 goal_Position;
+    private Vector3 goal_Rotation;
     private bool moving_To_Goal = false;
     private bool panning = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    public UnityEvent reached_Goal_Pos = new UnityEvent();
+
     void Start()
     {
-        click_Action.action.performed += Click_Recieved;
+        map_Click_Action.action.performed += Click_Recieved;
 
         cam = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Camera>();
 
         goal_Position = transform.position;
+        goal_Rotation = transform.forward;
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Vector3.Distance(goal_Position, transform.position) > 0.1f)
+        //If we're moving to a set location, do that
+        if ((Vector3.Distance(goal_Position, transform.position) > 0.1f) || (Vector3.Distance(goal_Rotation, transform.forward) > 0.1f))
         {
             moving_To_Goal = true;
             transform.position = Vector3.MoveTowards(transform.position, goal_Position, move_Speed * Time.deltaTime);
+            transform.forward = Vector3.RotateTowards(transform.forward, goal_Rotation, move_Speed * Time.deltaTime, move_Speed * Time.deltaTime);
+
+            if ((Vector3.Distance(goal_Position, transform.position) < 0.1f) && (Vector3.Distance(goal_Rotation, transform.forward) < 0.1f))
+            {
+                reached_Goal_Pos.Invoke();
+            }
         }
         else { moving_To_Goal = false; }
 
+        //If we're using a pointer to pan
         if ((!moving_To_Goal) && panning)
         {
-            if (click_Action.action.WasReleasedThisFrame()) { panning = false; }
+            if (map_Click_Action.action.IsPressed() == false) { panning = false; }
 
-            Vector2 pan_Input = cursor_Action.action.ReadValue<Vector2>();
+            Vector2 pan_Input = map_Cursor_Action.action.ReadValue<Vector2>();
 
             Vector3 panned_Position = new Vector3(transform.position.x - pan_Input.x, transform.position.y, transform.position.z - pan_Input.y);
 
@@ -55,18 +69,25 @@ public class Clicker_Player : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             IClickable clickable = hit.collider.GetComponent<IClickable>();
-            if (clickable == null) {panning = true;  return; }
+            if (clickable == null) { panning = true; return; }
 
             clickable.On_Click(this);
+            ///If we're already there, call the reached goal event now
+            if (Vector3.Distance(goal_Position, transform.position) < 0.1f) { reached_Goal_Pos.Invoke(); }
         }
-        else{ panning = true; }
-        
-        
+        else { panning = true; }
+
+
     }
 
 
     public void Set_Goal_Position(Vector3 new_Position)
     {
         goal_Position = new_Position;
+    }
+    
+    public void Set_Goal_Rotation(Vector3 new_Rotation)
+    {
+        goal_Rotation = new_Rotation;
     }
 }
--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -1,10 +1,57 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
 public class Location : MonoBehaviour, IClickable
 {
+    [SerializeField] Transform table_Camera_Transform;
+
+    private UIDocument document;
+    private Transform menu_Spawn; //This is an empty that represents where the sit down menu should spawn in world space
+    private InputActionReference click_Action;
+    private Clicker_Player clicker_Player;
+
+
+    private void Start()
+    {
+        document = gameObject.transform.GetChild(0).gameObject.GetComponent<UIDocument>();
+        menu_Spawn = gameObject.transform.GetChild(1);
+    }
     public void On_Click(Clicker_Player player)
     {
-        Debug.Log("Location clicked");
         player.Set_Goal_Position(new Vector3(transform.position.x, 0, transform.position.z));
+        player.reached_Goal_Pos.AddListener(On_Player_Reach_Pos);
+
+        this.click_Action = player.map_Click_Action;
+
+        clicker_Player = player;
+    }
+
+    private void On_Player_Reach_Pos()
+    {
+        document.enabled = true;
+        click_Action.action.performed += On_Unclick;
+        clicker_Player.reached_Goal_Pos.RemoveListener(On_Player_Reach_Pos);
+
+        VisualElement menu = document.rootVisualElement;
+        menu.transform.position = clicker_Player.cam.WorldToScreenPoint(menu_Spawn.position);
+
+        //call the sit down function when the button called that is clicked
+        (menu.Q("Sit_Down") as Button).RegisterCallback<ClickEvent>(Sit_Down);
+
+    }
+
+    private void On_Unclick(InputAction.CallbackContext context)
+    {
+        //document.enabled = false;
+        click_Action.action.performed -= On_Unclick;
+    }
+
+    private void Sit_Down(ClickEvent evt)
+    {
+        document.enabled = false;
+
+        clicker_Player.Set_Goal_Position(table_Camera_Transform.position);
+        clicker_Player.Set_Goal_Rotation(table_Camera_Transform.forward);
     }
 }
