8010fb7e84e5e0ad0d0c5ab105e3447e515523bb
probabl <hsfk2017@gmail.com>
2025-08-13 15:25:04 +0100

    Hardcoded fix to menu bug in previous commit
    I also confirmed the bug existed in a slightly less noticeable form in previous commits

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -43,12 +43,17 @@ public class Location : MonoBehaviour, IClickable
     private void On_Unclick(InputAction.CallbackContext context)
     {
 
-        Rect bounding_Rect = document.rootVisualElement.Q("Sit_Down").worldBound;
+        Rect bounding_Rect = document.rootVisualElement.worldBound;
+        //World bound was acting weird so I've just hardcoded the pixel size of the button here
+        //This is awful for Ui scalability, but it should work alright on most pc screens
+        bounding_Rect.width = 100;
+        bounding_Rect.height = 50;
+        bounding_Rect.y -= 50;
         Vector2 pos = Input.mousePosition;
         //If the click was outside of the menu's bounding box
         if (!bounding_Rect.Contains(pos))
         {
-            Debug.Log(document.rootVisualElement.transform);
+            Debug.Log(bounding_Rect + " " + pos);
             document.enabled = false;
             click_Action.action.performed -= On_Unclick;
         }
