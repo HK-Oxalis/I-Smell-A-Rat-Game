f9fae64b670a5e51f1a2fdca6bf6cf97d33c7dd7
retrograzer <noahgschultz@gmail.com>
2025-08-17 16:33:05 -0400

    Moved around some script files and got saving to JSON working

---
+++ Assets/Scripts/NotebookManager.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NotebookManager : MonoBehaviour
+{
+    public GameObject explanationDividerGO;
+    public Transform entryParent;
+    public TextMeshProUGUI entryNameTB;
+    public TextMeshProUGUI entryDiaRefTB;
+    public TextMeshProUGUI entryExplanationTB;
+
+    List<NotebookEntry> allEntries = new List<NotebookEntry>();
+
+    private void Awake()
+    {
+        explanationDividerGO.SetActive(false);
+        foreach (Transform index in entryParent)
+        {
+            allEntries.Add(index.gameObject.GetComponent<NotebookEntry>());
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LoadJSON();
+    }
+
+    void LoadJSON ()
+    {
+        // Load the JSON file (place it in Resources folder in Unity)
+        TextAsset jsonFile = Resources.Load<TextAsset>("SavedNotebook"); // no .json extension
+        Debug.Log("Opened JSON");
+
+        // Deserialize into C# objects
+        EntriesWrapper data = JsonUtility.FromJson<EntriesWrapper>(jsonFile.text);
+
+        foreach (Entry e in data.entries)
+        {
+            Debug.Log("Name: " + e.name + " Loaded");
+            //Debug.Log("Text: " + e.og_textbox);
+
+        }
+    }
+
+    public void DisplayExplanation (string entryName, string entryDialogueRef, string entryExplanation)
+    {
+        explanationDividerGO.SetActive(true);
+        entryNameTB.text = entryName;
+        entryDiaRefTB.text = entryDialogueRef;
+        entryExplanationTB.text = entryExplanation;
+    }
+}
+
+[Serializable]
+public class Entry
+{
+    public string name;
+    public string og_textbox;
+    public string explanation;
+    public List<string> connected_entries;
+}
+
+[Serializable]
+public class EntriesWrapper
+{
+    public List<Entry> entries;
+}
+
